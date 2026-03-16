1e83ece09c7c8271ca3e4b55de064ca32c1504bb
Anton811 <eduardoamg811@gmail.com>
2026-03-16 17:58:47 -0600

    Avance a dashboard y slideMenu

--- src/slidebarClient.js
+++ src/slidebarClient.js
@@ -1,3 +1,60 @@
 document.querySelector("#nav").innerHTML = `
-
+    <div class="offcanvas-header mt-3">
+        <img
+          src="/src/assets/PaseLogo.svg"
+          style="width: 60px; height: 60px"
+          viewBox="0 0 16 16"
+          alt=""
+        />
+        <h1 class="offcanvas-title">Menu</h1>
+        <button
+          class="btn-close text-reset"
+          data-bs-dismiss="offcanvas"
+        ></button>
+      </div>
+      <div class="offcanvas-body px-0">
+        <div class="container-fluid my-5">
+          <div class="d-flex justify-content-center">
+            <a href="" class="text-decoration-none d-flex"
+              ><img
+                src="/src/assets/Dashboard.svg"
+                style="width: 30px; height: 30px"
+                viewBox="0 0 16 16"
+              />
+              <h2 class="text-danger">Dashboard</h2></a
+            >
+          </div>
+        </div>
+        <div class="container-fluid my-5">
+          <div class="d-flex justify-content-center">
+            <a href="" class="text-decoration-none d-flex"
+              ><img
+                src="/src/assets/History.svg"
+                style="width: 30px; height: 30px"
+                viewBox="0 0 16 16"
+              />
+              <h2 class="text-danger">Mis Reservas</h2></a
+            >
+          </div>
+        </div>
+        <div class="container-fluid my-5">
+          <div class="d-flex justify-content-center">
+            <a href="" class="text-decoration-none d-flex"
+              ><img
+                src="/src/assets/Settings.svg"
+                style="width: 30px; height: 30px"
+                viewBox="0 0 16 16"
+              />
+              <h2 class="text-danger">Configuracion</h2></a
+            >
+          </div>
+        </div>
+        <div class="container-fluid my-5">
+          <div class="d-flex justify-content-center">
+            <button type="button" class="btn-danger btn fw-bold">
+              Cerrar Sesion
+            </button>
+          </div>
+        </div>
+      </div>
 `;
