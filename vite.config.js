b4a7d3ab6d210e7e2312b64e7f28a3069fd7ad02
Anton811 <eduardoamg811@gmail.com>
2026-05-11 16:24:48 -0600

    fix: Correct page input configuration for reserva and reservaAdmin in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,13 +7,13 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"), // Tu página principal
         dashboard: resolve(__dirname, "pages/client/dashboard/index.html"), // Tu página de dashboard,
-        reserva: resolve(__dirname, "pages/client/reserva/index.html"), //pagina para crear una reserva
         historial: resolve(__dirname, "pages/client/historial/index.html"),
         configuracion: resolve(__dirname, "pages/client/configuracion/index.html"),
         dashboardAdmin: resolve(__dirname, "pages/admin/dashboard/index.html"),
-        reserva: resolve(__dirname, "pages/admin/reservas/index.html"),
+        reservaAdmin: resolve(__dirname, "pages/admin/reservas/index.html"),
         clientes: resolve(__dirname, "pages/admin/cliente/index.html"),
         configuracionAdmin: resolve(__dirname, "pages/admin/configuracion/index.html"),
+        reserva: resolve(__dirname, "pages/client/reserva/index.html"),
         // Si tienes más páginas (reportes, admin), agrégalas aquí abajo
       },
     },
