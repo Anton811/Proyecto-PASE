48037a5dbcb01fedd6721724b5dc868afec2f4d7
Anton811 <eduardoamg811@gmail.com>
2026-05-11 14:37:06 -0600

    refactor: Clean up configuration file by removing unnecessary line breaks

--- vite.config.js
+++ vite.config.js
@@ -8,22 +8,12 @@ export default defineConfig({
         main: resolve(__dirname, "index.html"), // Tu página principal
         dashboard: resolve(__dirname, "pages/client/dashboard/index.html"), // Tu página de dashboard,
         reserva: resolve(__dirname, "pages/client/reserva/index.html"), //pagina para crear una reserva
-        reserva_confirmacion: resolve(
-          __dirname,
-          "pages/client/reserva/confirmacion/index.html",
-        ),
         historial: resolve(__dirname, "pages/client/historial/index.html"),
-        configuracion: resolve(
-          __dirname,
-          "pages/client/configuracion/index.html",
-        ),
+        configuracion: resolve(__dirname, "pages/client/configuracion/index.html"),
         dashboardAdmin: resolve(__dirname, "pages/admin/dashboard/index.html"),
         reserva: resolve(__dirname, "pages/admin/reservas/index.html"),
         clientes: resolve(__dirname, "pages/admin/cliente/index.html"),
-        configuracionAdmin: resolve(
-          __dirname,
-          "pages/admin/configuracion/index.html",
-        ),
+        configuracionAdmin: resolve(__dirname, "pages/admin/configuracion/index.html"),
         // Si tienes más páginas (reportes, admin), agrégalas aquí abajo
       },
     },
